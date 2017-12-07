b4e98d8c455a7d62f57febffc70c839f3ed87541
Dreadds <kevinatc10@gmail.com>
2017-12-07 18:39:32 -0500

    lista

--- src/pages/tab1/tab1.ts
+++ src/pages/tab1/tab1.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { PERSONAJES} from "../../data/personajes.data";
+import { Personaje} from "../../interfaces/personaje.interface";
 
 /**
  * Generated class for the Tab1Page page.
@@ -15,7 +17,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class Tab1Page {
 
+  personajes: Personaje[] = [];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.personajes = PERSONAJES.slice(0);
   }
 
   ionViewDidLoad() {
